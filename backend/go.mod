a8506089e53b6c5ed604b5ffc749e26152f9312c
nafiz1001 <nafiz.islam1001@gmail.com>
2021-12-20 15:20:09 -0500

    improve unit test for account model

--- backend/go.mod
+++ backend/go.mod
@@ -9,6 +9,14 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
+
+require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.4 // indirect
 	gorm.io/driver/sqlite v1.1.5
--- backend/model/account_test.go
+++ backend/model/account_test.go
@@ -3,12 +3,15 @@ package model
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/nafiz1001/gallery-go/dto"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func DBInit(t *testing.T) AccountDB {
+func AccountDBInit(t *testing.T) AccountDB {
 	gormDB, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 	if err != nil {
 		t.Fatal(err)
@@ -18,31 +21,26 @@ func DBInit(t *testing.T) AccountDB {
 
 	var accountDB AccountDB
 	err = accountDB.Init(&db)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	return accountDB
 }
 
-func CreateAccount(t *testing.T, db AccountDB, username string, password string) *dto.AccountDto {
+func CreateAccount(t *testing.T, db AccountDB, username string, password string) dto.AccountDto {
 	dto := dto.AccountDto{
 		Username: username,
 		Password: password,
 	}
 
 	account, err := db.CreateAccount(dto)
-	if err != nil {
-		t.Fatal(err)
-	} else if account == nil {
-		t.Fatalf("account nil when creating account with username '%s'", dto.Username)
-	}
+	require.NoError(t, err)
+	require.NotNil(t, account)
 
-	return account
+	return *account
 }
 
 func TestAccountDBInit(t *testing.T) {
-	db := DBInit(t)
+	db := AccountDBInit(t)
 	defer func() {
 		otherDb, _ := db.db.DB()
 		otherDb.Close()
@@ -50,7 +48,7 @@ func TestAccountDBInit(t *testing.T) {
 }
 
 func TestCreateAccount(t *testing.T) {
-	db := DBInit(t)
+	db := AccountDBInit(t)
 	defer func() {
 		otherDb, _ := db.db.DB()
 		otherDb.Close()
@@ -58,34 +56,29 @@ func TestCreateAccount(t *testing.T) {
 
 	// successful create
 	account1 := CreateAccount(t, db, "username", "password")
+	assert.Equal(t, account1.Username, "username")
 
 	// duplicate username
 	account2, err := db.CreateAccount(dto.AccountDto{
 		Username: "username",
 		Password: "password",
 	})
-	if err == nil {
-		t.Fatalf("err should be nil for identical username '%s'", account2.Username)
-	} else if account2 != nil {
-		t.Fatalf("account2 should be nil for identical username '%s'", account2.Username)
-	}
+	assert.Error(t, err)
+	assert.Nil(t, account2)
 
 	// successful second create
-	account2, _ = db.CreateAccount(dto.AccountDto{
+	account2, err = db.CreateAccount(dto.AccountDto{
 		Username: "username2",
 		Password: "password2",
 	})
-	if account2 != nil {
-		if account2.Id == account1.Id {
-			t.Fatalf("expected to not create account with identical id '%d'", account2.Id)
-		} else if account2.Username == account1.Username {
-			t.Fatalf("expected to not create account with identical username '%s'", account2.Username)
-		}
+	if assert.NoError(t, err) && assert.NotNil(t, account2) {
+		assert.NotEqual(t, account2.Id, account1.Id)
+		assert.NotEqual(t, account2.Username, account1.Username)
 	}
 }
 
 func TestGetAccountById(t *testing.T) {
-	db := DBInit(t)
+	db := AccountDBInit(t)
 	account := CreateAccount(t, db, "username", "password")
 	defer func() {
 		otherDb, _ := db.db.DB()
@@ -93,26 +86,19 @@ func TestGetAccountById(t *testing.T) {
 	}()
 
 	// successful get account by id
-	dto, err := db.GetAccountById(account.Id)
-	if err != nil {
-		t.Fatal(err)
-	} else if dto == nil {
-		t.Fatalf("dto returned nil for id '%d'", account.Id)
-	} else if account.Id != dto.Id {
-		t.Fatalf("id returned '%d' does not match dto's id '%d'", account.Id, dto.Id)
+	if dto, err := db.GetAccountById(account.Id); assert.NoError(t, err) && assert.NotNil(t, dto) {
+		assert.Equal(t, dto.Id, account.Id)
+		assert.Equal(t, dto.Username, account.Username)
 	}
 
 	// get account by non-existent id
-	dto, err = db.GetAccountById(420)
-	if err == nil {
-		t.Fatalf("error not nil for non-existent id '%d'", 420)
-	} else if dto != nil {
-		t.Fatalf("dto returned for non-existent id '%d'", 420)
-	}
+	dto, err := db.GetAccountById(420)
+	assert.Error(t, err)
+	assert.Nil(t, dto)
 }
 
 func TestGetAccountByUsername(t *testing.T) {
-	db := DBInit(t)
+	db := AccountDBInit(t)
 	defer func() {
 		otherDb, _ := db.db.DB()
 		otherDb.Close()
@@ -121,19 +107,13 @@ func TestGetAccountByUsername(t *testing.T) {
 
 	// successful get account by username
 	dto, err := db.GetAccountByUsername(account.Username)
-	if err != nil {
-		t.Fatal(err)
-	} else if account == nil {
-		t.Fatalf("account nil for existing username '%s'", account.Username)
-	} else if account.Username != dto.Username {
-		t.Fatalf("dto's username '%s' does not match username '%s", account.Username, dto.Username)
+	if assert.NoError(t, err) && assert.NotNil(t, dto) {
+		assert.Equal(t, dto.Id, account.Id)
+		assert.Equal(t, dto.Username, dto.Username)
 	}
 
 	// get account by non-existent username
 	dto, err = db.GetAccountByUsername("username2")
-	if err == nil {
-		t.Fatalf("error is nil for non-existent username")
-	} else if dto != nil {
-		t.Fatalf("dto returned for non-existent username")
-	}
+	assert.Error(t, err)
+	assert.Nil(t, dto)
 }
